3885bc4c8a09068df0d126e75e8f209cb69da838
VooDooS <thevoodoos@gmail.com>
2019-08-12 16:05:42 +0200

    Add taxonomy Types in theme

--- web/app/themes/twentynineteen-child/functions.php
+++ web/app/themes/twentynineteen-child/functions.php
@@ -7,6 +7,42 @@ function theme_enqueue_styles() {
 }
 
 /**
+ * TAXONOMIES
+ */
+function cptui_register_my_taxes() {
+
+	/**
+	 * Taxonomy: Resource types.
+	 */
+
+	$labels = array(
+		"name" => __( "Resource types", "twenty-nineteen-child" ),
+		"singular_name" => __( "Resource type", "twenty-nineteen-child" ),
+	);
+
+	$args = array(
+		"label" => __( "Resource types", "twenty-nineteen-child" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'res_types', 'with_front' => true, ),
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "res_types",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "res_types", array( "res" ), $args );
+}
+add_action( 'init', 'cptui_register_my_taxes' );
+
+
+/**
  * POST TYPES
  */
 function cptui_register_my_cpts() {
