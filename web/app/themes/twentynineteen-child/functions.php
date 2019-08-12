5f9a6a346fc98a6c514a25a92a805b0246686d4a
VooDooS <thevoodoos@gmail.com>
2019-08-12 16:36:15 +0200

    Working on custom posts/ taxon

--- web/app/themes/twentynineteen-child/functions.php
+++ web/app/themes/twentynineteen-child/functions.php
@@ -1,27 +1,28 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 
 /**
  * TAXONOMIES
  */
-function cptui_register_my_taxes() {
+function cptui_register_my_taxes()
+{
 
 	/**
 	 * Taxonomy: Resource types.
 	 */
 
 	$labels = array(
-		"name" => __( "Resource types", "twenty-nineteen-child" ),
-		"singular_name" => __( "Resource type", "twenty-nineteen-child" ),
+		"name" => __("Resource types", "twenty-nineteen-child"),
+		"singular_name" => __("Resource type", "twenty-nineteen-child"),
 	);
 
 	$args = array(
-		"label" => __( "Resource types", "twenty-nineteen-child" ),
+		"label" => __("Resource types", "twenty-nineteen-child"),
 		"labels" => $labels,
 		"public" => true,
 		"publicly_queryable" => true,
@@ -30,34 +31,35 @@ function cptui_register_my_taxes() {
 		"show_in_menu" => true,
 		"show_in_nav_menus" => true,
 		"query_var" => true,
-		"rewrite" => array( 'slug' => 'res_types', 'with_front' => true, ),
+		"rewrite" => array('slug' => 'res_types', 'with_front' => true,),
 		"show_admin_column" => false,
 		"show_in_rest" => true,
 		"rest_base" => "res_types",
 		"rest_controller_class" => "WP_REST_Terms_Controller",
 		"show_in_quick_edit" => false,
-		);
-	register_taxonomy( "res_types", array( "res" ), $args );
+	);
+	register_taxonomy("res_types", array("res"), $args);
 }
-add_action( 'init', 'cptui_register_my_taxes' );
+add_action('init', 'cptui_register_my_taxes');
 
 
 /**
  * POST TYPES
  */
-function cptui_register_my_cpts() {
+function cptui_register_my_cpts()
+{
 
 	/**
 	 * Post Type: Resources.
 	 */
 
 	$labels = array(
-		"name" => __( "Resources", "twenty-nineteen-child" ),
-		"singular_name" => __( "Resource", "twenty-nineteen-child" ),
+		"name" => __("Resources", "twenty-nineteen-child"),
+		"singular_name" => __("Resource", "twenty-nineteen-child"),
 	);
 
 	$args = array(
-		"label" => __( "Resources", "twenty-nineteen-child" ),
+		"label" => __("Resources", "twenty-nineteen-child"),
 		"labels" => $labels,
 		"description" => "",
 		"public" => true,
@@ -74,25 +76,25 @@ function cptui_register_my_cpts() {
 		"capability_type" => "post",
 		"map_meta_cap" => true,
 		"hierarchical" => false,
-		"rewrite" => array( "slug" => "res", "with_front" => true ),
+		"rewrite" => array("slug" => "res", "with_front" => true),
 		"query_var" => true,
 		"menu_icon" => "dashicons-screenoptions",
-		"supports" => array( "title", "editor", "thumbnail" ),
+		"supports" => array("title", "editor", "thumbnail"),
 	);
 
-	register_post_type( "res", $args );
+	register_post_type("res", $args);
 
 	/**
 	 * Post Type: Creators.
 	 */
 
 	$labels = array(
-		"name" => __( "Creators", "twenty-nineteen-child" ),
-		"singular_name" => __( "Creator", "twenty-nineteen-child" ),
+		"name" => __("Creators", "twenty-nineteen-child"),
+		"singular_name" => __("Creator", "twenty-nineteen-child"),
 	);
 
 	$args = array(
-		"label" => __( "Creators", "twenty-nineteen-child" ),
+		"label" => __("Creators", "twenty-nineteen-child"),
 		"labels" => $labels,
 		"description" => "The resources\' creators",
 		"public" => true,
@@ -109,15 +111,37 @@ function cptui_register_my_cpts() {
 		"capability_type" => "post",
 		"map_meta_cap" => true,
 		"hierarchical" => false,
-		"rewrite" => array( "slug" => "creator", "with_front" => true ),
+		"rewrite" => array("slug" => "creator", "with_front" => true),
 		"query_var" => true,
 		"menu_icon" => "dashicons-groups",
-		"supports" => array( "title", "editor", "thumbnail" ),
-		"taxonomies" => array( "post_tag" ),
+		"supports" => array("title", "editor", "thumbnail"),
+		"taxonomies" => array("post_tag"),
 	);
 
-	register_post_type( "creator", $args );
+	register_post_type("creator", $args);
 }
 
-add_action( 'init', 'cptui_register_my_cpts' );
+add_action('init', 'cptui_register_my_cpts');
 
+/**
+ * Taxonomy term
+ */
+$ty_link = wp_insert_term(
+	'Link', // the term
+	'product', // the taxonomy
+	array(
+		'description' => 'A Link-based resource.',
+		'slug' => 'link',
+		//'parent'=> $parent_term['term_id']  // get numeric term id
+	)
+);
+
+$ty_link = wp_insert_term(
+	'Lien', // the term
+	'res_types', // the taxonomy
+	array(
+		'description' => 'A Link-based resource.',
+		'slug' => 'link-fr',
+		//'parent'=> $parent_term['term_id']  // get numeric term id
+	)
+);
