a7e2c44a1f4f3844d91ff005ccc7acb6b40c5e87
VooDooS <thevoodoos@gmail.com>
2019-08-12 15:43:14 +0200

    Add temporary child theme

---
+++ web/app/themes/twentynineteen-child/functions.php
@@ -0,0 +1,87 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+/**
+ * POST TYPES
+ */
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: Resources.
+	 */
+
+	$labels = array(
+		"name" => __( "Resources", "twenty-nineteen-child" ),
+		"singular_name" => __( "Resource", "twenty-nineteen-child" ),
+	);
+
+	$args = array(
+		"label" => __( "Resources", "twenty-nineteen-child" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "res", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-screenoptions",
+		"supports" => array( "title", "editor", "thumbnail" ),
+	);
+
+	register_post_type( "res", $args );
+
+	/**
+	 * Post Type: Creators.
+	 */
+
+	$labels = array(
+		"name" => __( "Creators", "twenty-nineteen-child" ),
+		"singular_name" => __( "Creator", "twenty-nineteen-child" ),
+	);
+
+	$args = array(
+		"label" => __( "Creators", "twenty-nineteen-child" ),
+		"labels" => $labels,
+		"description" => "The resources\' creators",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "creator", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-groups",
+		"supports" => array( "title", "editor", "thumbnail" ),
+		"taxonomies" => array( "post_tag" ),
+	);
+
+	register_post_type( "creator", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
+
